e79e77a6875820955c4b8d7042fdfe0b207c9c6f
ColdView <vinnimacc@mail.com>
2017-06-06 21:35:32 +0100

    First commit of finished sentdex Django tutorial and part of Django Docs tutorial

---
+++ sentdex/mysite/personal/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content': ['To contact me email at', 'someemail@mail.com']})
